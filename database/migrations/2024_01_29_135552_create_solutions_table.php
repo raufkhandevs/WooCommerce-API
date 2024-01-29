aa870e11f2d38bf39c4ea4897cb4aa08a16e7d69
raufkhandevs <raufkhandevs@gmail.com>
2024-01-29 18:58:00 +0500

    admin panel setup

---
+++ database/migrations/2024_01_29_135552_create_solutions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('solutions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->json('solution_videos');
+            $table->unsignedBigInteger('wp_product_id');
+            $table->foreign('wp_product_id')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('solutions');
+    }
+};
